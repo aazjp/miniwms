d175c40a0ccf2d43fb2cfa45bdf7657adfbff9ae
Zhou JunPeng <1031198425@qq.com>
2025-03-05 16:20:46 +0800

    修改了搜索客户的url

--- customer/urls.py
+++ customer/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('customer_manage/', views.customer_manage, name='customer_manage'),
     path('customer_del/<customer_id>', views.customer_del, name='customer_del'),
     path('customer_edit/<customer_id>', views.customer_edit, name='customer_edit'),
-    path('search_customer/', views.search_customer, name='search_customer'),
 ]
