774331e44e86de14e069f46295a28a744604f775
Zhou JunPeng <1031198425@qq.com>
2025-03-05 11:59:51 +0800

    删除clear_database

--- clear_database.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-for /f "delims=" %%a in ('dir /b /ad /s ^| findstr /i /e "\migrations"') do (
-    for /r "%%a" %%i in (*) do (
-        @findstr /m "initial" "%%i" && del "%%i"
-    )
-)
