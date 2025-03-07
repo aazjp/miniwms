bee8155e4119a33ca27540e4723b4df68cc876b0
周君鹏 <14378381+zhoujp1031198425@user.noreply.gitee.com>
2025-03-08 01:34:49 +0800

    header:新增了用户名显示 style:新增了根据权限显示spu列表

--- main/middleware.py
+++ main/middleware.py
@@ -7,7 +7,7 @@ class LoginRequiredMiddleware:
 
     def __call__(self, request):
         # 定义不需要登录验证的 URL 列表
-        exempt_urls = [reverse('main:login'), ]  # 'login' 是登录视图的 URL 名称
+        exempt_urls = [reverse('main:login'), reverse('main:register_view')]  # 'login' 是登录视图的 URL 名称
         if not request.user.is_authenticated and request.path_info not in exempt_urls:
             return redirect('main:login')
         response = self.get_response(request)
